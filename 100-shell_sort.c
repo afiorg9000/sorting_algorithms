3321a6989b14721c3344da6dcd8242ad3234ed45
afiorg9000 <3533@holbertonschool.com>
2022-02-15 14:46:09 +0000

    psuh

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,31 +8,32 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = sizeof(size), idx;
+	size_t n = 1, idx, j;
+	int tmp;
 
 	if (size < 2 || array == NULL)
 	{
 		return;
 	}
-
-	while (gap < (size / 3))
-		gap = gap * 3 + 1;
-	/* finds the max gap*/
+	while (n < size)
 	{
-		for (; gap > 0; gap /= 3)
-			/*decreases gap sequence*/
-			/*compares values from begining and from gap*/
+		n = n * 3 + 1;
+	}
+	n /= 3;
+	while (n > 0)
+	{
+		/* finds the max gap*/
+		/*decreases gap sequence*/
+		/*compares values from begining and from gap*/
+		for (idx = n; idx < size; idx ++)
 		{
-
-			for (idx = gap; idx < size; idx++)
-			{
-				for (; idx >= gap && array[idx - gap] > array[idx]; idx -= gap)
-				{
-					array[idx] = array[idx - gap];
-				}
-				array[idx] = array[idx];
-			}
-			print_array(array, size);
+			tmp = array[idx];
+			for (j = idx; j >= n && array[j - n] > tmp; j -= n)
+				array[j] = array[j - n];
+			array[j] = tmp;
 		}
+		n /= 3;
+		print_array(array, size);
 	}
 }
+
